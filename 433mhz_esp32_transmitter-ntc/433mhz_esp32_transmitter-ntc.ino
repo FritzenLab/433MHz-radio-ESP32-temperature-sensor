5960d150d9e4587fc273818adc5edde5c340cbfb
Clovis Fritzen <clovisf@gmail.com>
2025-03-22 19:01:11 -0300

    edited code

--- 433mhz_esp32_transmitter-ntc/433mhz_esp32_transmitter-ntc.ino
+++ 433mhz_esp32_transmitter-ntc/433mhz_esp32_transmitter-ntc.ino
@@ -18,7 +18,7 @@ void setup()
 
 void loop()
 {	
-  analogvalue= analogRead(A0);
+  analogvalue= analogRead(A1); // calling it pin "2" instead of A0, pin "3" instead of A1
   rfsense.send(analogvalue, 12);
   Serial.println(analogvalue);
   delay(1000);
